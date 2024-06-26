95589d38ddae49d091f789c10aa96cdc1387f754
D <adacountesslovelace@gmail.com>
2024-06-26 00:22:21 -0700

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/lnurlp/:user',
+        destination: '/api/lnurlp', // Matched parameters can be used in the destination
+      },
+      // add clean urls
+      {
+        source: "/:path*",
+        destination: "/:path*.html",
+      },
+    ]
+  }
+};
 
 export default nextConfig;
