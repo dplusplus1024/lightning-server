3e4a129371d5962851b12eb5edc8b3bf03231ba1
Dread <34528298+islandbitcoin@users.noreply.github.com>
2024-08-21 15:28:13 -0400

    update www

--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,30 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/.well-known/lnurlp/:user',
-        destination: '/api/lnurlp/:user',
-      }
+        source: "/.well-known/lnurlp/:user",
+        destination: "/api/lnurlp/:user",
+      },
     ];
   },
   async redirects() {
     return [
       // Main homepage
       {
-        source: '/',
-        destination: '/index.html',
+        source: "/",
+        destination: "/index.html",
+        permanent: true,
+      },
+      {
+        source: "/",
+        destination: "/index.html",
+        has: [{ type: "host", value: "www.islandbitcoin.com" }],
         permanent: true,
       },
       // Trezor Academy subdomain
       {
-        source: '/',
-        destination: 'https://trezoracademy.rsvpify.com',
-        has: [{ type: 'host', value: 'trezoracademy.islandbitcoin.com' }],
+        source: "/",
+        destination: "https://trezoracademy.rsvpify.com",
+        has: [{ type: "host", value: "trezoracademy.islandbitcoin.com" }],
         permanent: false,
       },
     ];
